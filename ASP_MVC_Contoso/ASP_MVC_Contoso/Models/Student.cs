75842e3b7d9a9ca980d653a8821586a4950e5fc3
Arbaaz Patwari <arbaazpatwari@gmail.com>
2022-12-18 10:28:39 +0000

    testing crud pages

--- ASP_MVC_Contoso/ASP_MVC_Contoso/Models/Student.cs
+++ ASP_MVC_Contoso/ASP_MVC_Contoso/Models/Student.cs
@@ -7,7 +7,9 @@ namespace ASP_MVC_Contoso.Models
     public class Student
     {
         public int StudentID { get; set; }
+        [StringLength(30)]
         public string LastName { get; set; }
+        [StringLength(30)]
         public string FirstName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
